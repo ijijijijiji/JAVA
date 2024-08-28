5e508b22eaf6d171f8e0ade2e5e23f7d12ba1f83
seoyoun0823 <129851513+ijijijijiji@users.noreply.github.com>
2024-08-28 16:20:17 +0900

    Update Static2.java

--- java 방과후/변수 타입/Static2.java
+++ java 방과후/변수 타입/Static2.java
@@ -14,17 +14,17 @@ public class Static2 {
 	public static void main(String[] args) {
 		
 		Student9 n1 = new Student9("John");
-		System.out.println("1static count: " + n1.count9 + " count9: " + n1.count9);
+		System.out.println("1static count: " + n1.count + " count9: " + n1.count9);
 		
 		Student9 n2 = new Student9("Nim");
-		System.out.println("2static count: " + n2.count9 + " count9: " + n2.count9);
+		System.out.println("2static count: " + n2.count + " count9: " + n2.count9);
 		
 		Student9 n3 = new Student9("Sun");
-		System.out.println("3static count: " + n3.count9 + " count9: " + n3.count9);
+		System.out.println("3static count: " + n3.count + " count9: " + n3.count9);
 		
 		// 클래스(static) 변수와 인스턴스 변수의 차이점
 		System.out.println("===========================");
-		n1.count9 += n1.count;
+		n1.count += n1.count;
 		n1.count9 += n1.count9;
 		
 		System.out.println("4static count: " + n1.count + " count9: " + n1.count9);
