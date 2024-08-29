e39db1bcc7b7a236f2c73750603dd75bcdedab8d
seoyoun0823 <129851513+ijijijijiji@users.noreply.github.com>
2024-08-29 18:14:12 +0900

    Update S21200_01v4.java

--- java 방과후/타입 변환/S21200_01v4.java
+++ java 방과후/타입 변환/S21200_01v4.java
@@ -11,3 +11,18 @@ public class S21200_01v4 {
 		System.out.println("result: " + result);
 	}
 }
+
+
+package S21200_java00;
+
+public class S21200_01v4 {
+
+	public static void main(String[] args) {
+		
+		int x = 1;
+		int y = 2;
+		double result = (double) x/y;
+		
+		System.out.println("result: " + result);
+	}
+}
